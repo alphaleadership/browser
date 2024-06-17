9dce9d326fa25a6a2b5ea6297cfaa2edfc959fe3
alphaleadership <thomas.iniguez@free.fr>
2024-06-17 10:29:01 +0200

    tailwind

--- front-end/preload.js
+++ front-end/preload.js
@@ -1,12 +1,6 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('api', {
-  makeRequest: (options) => ipcRenderer.invoke('make-request', options),
-  getCookies: (url) => ipcRenderer.invoke('get-cookies', url),
-  setCookie: (cookie) => ipcRenderer.invoke('set-cookie', cookie),
-  removeCookie: (url, name) => ipcRenderer.invoke('remove-cookie', url, name),
-  savePassword: (passwordData) => ipcRenderer.invoke('save-password', passwordData),
-  getPasswords: (url) => ipcRenderer.invoke('get-passwords', url),
 
   // Fonction pour ouvrir une nouvelle webview pour les popups généraux
   openPopup: (url) => {
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./front-end/index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
